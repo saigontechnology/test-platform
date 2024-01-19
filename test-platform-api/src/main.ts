f6a5b134522479c3ea57e6273ccdab38ecb1b7de
Khanh Nguyen <khanh.nguyen@saigontechnology.com>
2024-01-19 14:24:24 +0700

    Add swagger

--- test-platform-api/src/main.ts
+++ test-platform-api/src/main.ts
@@ -7,14 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  // const config = new DocumentBuilder()
-  //   .setTitle('Test Platform')
-  //   .setDescription('Test Platform API description')
-  //   .setVersion('1.0')
-  //   .build();
+  const config = new DocumentBuilder()
+    .setTitle('Test Platform')
+    .setDescription('Test Platform API description')
+    .setVersion('1.0')
+    .build();
 
-  // const document = SwaggerModule.createDocument(app, config);
-  // SwaggerModule.setup('api', app, document);
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT);
 }
