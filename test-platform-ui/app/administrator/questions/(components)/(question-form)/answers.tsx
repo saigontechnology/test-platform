affe5fa34e695e2d6b35d0879eb2e7cdd0ef040f
datngo21 <dat.ngo@saigontechnology.com>
2024-02-05 17:00:51 +0700

    Test Platform: Update code

--- test-platform-ui/app/administrator/questions/(components)/(question-form)/answers.tsx
+++ test-platform-ui/app/administrator/questions/(components)/(question-form)/answers.tsx
@@ -101,7 +101,7 @@ const RenderQuestionAnswers = (props: IQuestionAnswers): ReactElement => {
             id="default-checkbox"
             type="checkbox"
             checked={answ.isCorrect}
-            className="h-4 w-4 rounded border-gray-300 bg-gray-100 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
+            className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-2 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-blue-600"
             onChange={() => HandleInteractions.handleSelectCorrect(answ)}
           />
         ) : null}
