c9df81bbd6bb7395e9c5d388cc9850fde9b480e9
Khanh Nguyen <khanh.nguyen@saigontechnology.com>
2024-02-21 16:45:08 +0700

    Update invitation link

--- test-platform-api/src/utils/mailer.ts
+++ test-platform-api/src/utils/mailer.ts
@@ -19,6 +19,6 @@ export const inviteExamination = async (
     from: process.env.SENDER_EMAIL,
     to: receiver,
     subject: "Invite to do exam",
-    text: `Your examination is: ${process.env.CLIENT_WEB_URL}/examination/${examinationId}`,
+    text: `Your examination is: ${process.env.CLIENT_WEB_URL}/exam_invitation/${examinationId}`,
   });
 };
