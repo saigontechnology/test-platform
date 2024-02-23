6259c10689aa788425206b55e7f2350a933f440c
datngo21 <dat.ngo@saigontechnology.com>
2024-02-23 15:39:21 +0700

    Test Platform: Update scoring server-side

--- test-platform-api/src/examination/dto/update-examination.dto.ts
+++ test-platform-api/src/examination/dto/update-examination.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
-import { IsArray, ValidateNested } from "class-validator";
+import { IsArray, IsNumber, IsString, ValidateNested } from "class-validator";
 import { ExaminationAnswer } from "./create-examination.dto";
 
 export class UpdateExaminationDto {
@@ -9,4 +9,12 @@ export class UpdateExaminationDto {
   @Type(() => ExaminationAnswer)
   @ApiProperty({ required: true, isArray: true, type: ExaminationAnswer })
   selections: ExaminationAnswer[];
+
+  @IsNumber()
+  @ApiProperty({ required: true })
+  assessmentId: number;
+
+  @IsString()
+  @ApiProperty({ required: true })
+  email: string;
 }
--- test-platform-api/src/examination/examinations.module.ts
+++ test-platform-api/src/examination/examinations.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
+import { AssessmentsService } from "src/assessments/assessment.service";
+import { PrismaModule } from "src/prisma/prisma.module";
 import { ExaminationsService } from "./examination.service";
 import { ExaminationsController } from "./examinations.controller";
-import { PrismaModule } from "src/prisma/prisma.module";
 
 @Module({
   controllers: [ExaminationsController],
-  providers: [ExaminationsService],
+  providers: [ExaminationsService, AssessmentsService],
   imports: [PrismaModule],
 })
 export class ExaminationsModule {}
